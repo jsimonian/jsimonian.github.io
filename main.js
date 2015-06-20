8c78d8542fb82c0c206d0914e0ed592639256ee6
Joseph Simonian <j.simonian10@gmail.com>
2015-06-19 21:36:18 -0700

    Image Fade

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
   $('div#bg').hide();
-  $("<img/>").attr("src", "./images/skylinegrad.png").load(function() {
+  $(".social-links").css("opacity", "0");
+  $(".social-links").css("margin", "50px");
+  $("<img/>").attr("src", "./images/skylinegrad.png").load(function() { // make a "dummy" element to get access to the .load() event for the background image
     $(this).remove();
-    $("div#bg").fadeIn(5000);
+    $("div#bg").fadeIn(3000);
+
+    $(".social-links").animate({
+      margin: 0,
+      opacity: 1
+    }, 3000, function() {});
   });
-  // if ($('div#bg')[0].complete) {
-  //   $('div#bg').fadeIn();
-  // }
-  // else {
-  //   $('div#bg').load(function() {
-  //     $('div#bg').fadeIn();
-  //   });
-  // }
 });
