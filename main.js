c793345a603a7b47098ad523bb5316902344e10d
Joseph Simonian <j.simonian10@gmail.com>
2015-06-20 16:40:12 -0700

    add dummy element

--- main.js
+++ main.js
@@ -1,25 +1,14 @@
-// $(document).ready(function() {
-//   $('div#bg').hide();
-//   $("#social-links").css("opacity", "0");
-//   $("#social-links").css("margin", "50px");
-//   $("<img/>").attr("src", "./images/skylinegrad.png").load(function() { // make a "dummy" element to get access to the .load() event for the background image
-//     $(this).remove();
-//     $("div#bg").fadeIn(3000);
-//
-//     $("#social-links").animate({
-//       margin: 0,
-//       opacity: 1
-//     }, 3000, function() {});
-//   });
-// });
-
 $(document).ready(function() {
   $('div#bg').hide();
   $("#social-links").css("opacity", "0");
   $("#social-links").css("margin", "50px");
-  $("div#bg").fadeIn(3000);
-  $("#social-links").animate({
-    margin: 0,
-    opacity: 1
-  }, 3000, function() {});
+  $("<img/>").attr("src", "./images/skylinegrad.png").load(function() { // make a "dummy" element to get access to the .load() event for the background image
+    $(this).remove();
+    $("div#bg").fadeIn(3000);
+
+    $("#social-links").animate({
+      margin: 0,
+      opacity: 1
+    }, 3000, function() {});
+  });
 });
