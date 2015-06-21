36a00a905e0a8d1c0c4335ff9af2fd9f749ed2bf
Joseph Simonian <j.simonian10@gmail.com>
2015-06-21 15:07:02 -0700

    Many

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
-  $('div#bg').hide();
+  $("div#bg").css("opacity", "0");
   $("#social-links").css("opacity", "0");
   $("#social-links").css("margin", "50px");
+
   $("div#bg img").load(function() {
-    $("div#bg").fadeIn(3000);
-    $("#social-links").animate({
+    $("div#bg").animate({
+      opacity: 1
+    }, 3000, function() {});
+
+  $("#social-links").animate({
       margin: 0,
       opacity: 1
     }, 3000, function() {});
+
   });
 });
